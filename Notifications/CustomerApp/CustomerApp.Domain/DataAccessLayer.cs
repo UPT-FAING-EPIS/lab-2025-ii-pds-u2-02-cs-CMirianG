5a49ef181888e60c64f1230b5ac5145d4ebd4b75
CMirianG <mc2021071083@virtual.upt.pe>
2025-10-27 01:25:45 -0500

    cambios_v1

---
+++ Notifications/CustomerApp/CustomerApp.Domain/DataAccessLayer.cs
@@ -0,0 +1,17 @@
+namespace CustomerApp.Domain
+{
+    public class DataAccessLayer
+    {
+        public List<Customer> Customers { get; set; }
+        public DataAccessLayer()
+        {
+            Customers = new List<Customer>();
+        }
+        public bool SaveCustomer(Customer customer)
+        {
+            Customers.Add(customer);
+            return true;
+        }        
+    }
+}
+
